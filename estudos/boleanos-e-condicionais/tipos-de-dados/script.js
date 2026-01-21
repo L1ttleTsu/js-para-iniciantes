06c0416f22480180491366d096789b8165d60d28
Bruno Gonçalves Santos <littletsu28@gmail.com>
2026-01-21 16:29:27 -0300

    Adição de novos exercicios e o inicio de um projeto em JS

---
+++ estudos/boleanos-e-condicionais/tipos-de-dados/script.js
@@ -0,0 +1,12 @@
+var possueFaculdade = true;
+if(possueFaculdade) {
+    console.log("Parabéns por ter uma faculdade!")
+}
+else {
+    console.log("Faça uma faculdade quando puder!")
+}
+// o que retorna false
+// if(false)
+// if(0)
+// if(NaN)
+// if(null)
