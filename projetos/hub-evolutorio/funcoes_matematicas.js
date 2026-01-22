b92175ecb00854f919f401f1a88638ca2e58e4c7
Bruno Gonçalves Santos <littletsu28@gmail.com>
2026-01-22 18:49:30 -0300

    Melhoria na organizção de páginas

--- projetos/hub-evolutorio/funcoes_matematicas.js
+++ projetos/hub-evolutorio/funcoes_matematicas.js
@@ -1,12 +1,16 @@
 function calcularFuncaoDoPrimeiroGrau() {
-
+    const valorDeEntrada = Number(document.getElementById("valorDeEntrada").value);
+    const coeficienteAngular = Number(document.getElementById("coeficienteAngular").value);
+    const termoIndependente = Number(document.getElementById("termoindependente").value);
+    const resultadoDaFuncao = coeficienteAngular * valorDeEntrada + termoIndependente;
+    document.getElementById("resultadoDaFuncao").textContent = `O resultado é ${resultadoDaFuncao}`
 }
 
 function calcularFuncaoQuadratica() {
-    const valorDeEntrada = Number(document.getElementById("valorDeEntrada").value);
+    const valorDeEntradaDaFuncaoQuadratica = Number(document.getElementById("valorDeEntradaDaFuncaoQuadratica").value);
     const coeficienteQuadratico = Number(document.getElementById("coeficienteQuadratico").value);
     const coeficienteLinear = Number(document.getElementById("coeficienteLinear").value);
-    const termoIndependente = Number(document.getElementById("termoIndependente").value);
-    const resultadoDaFuncao = coeficienteQuadratico * (valorDeEntrada ** 2) + coeficienteLinear * valorDeEntrada + termoIndependente;
-    document.getElementById("resultadoDaFuncao").textContent = resultadoDaFuncao;
+    const termoIndependenteDaFuncaoQuadratica = Number(document.getElementById("termoIndependenteDaFuncaoQuadratica").value);
+    const resultadoDaFuncao = coeficienteQuadratico * (valorDeEntradaDaFuncaoQuadratica ** 2) + coeficienteLinear * valorDeEntradaDaFuncaoQuadratica + termoIndependenteDaFuncaoQuadratica;
+    document.getElementById("resultadoDaFuncaoQuadratica").textContent = `O resultado é ${resultadoDaFuncao}`;
 }
