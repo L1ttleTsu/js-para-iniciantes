624dc7082ce6777a220d75d7fb2dd83268ecd27a
Bruno Gonçalves Santos <littletsu28@gmail.com>
2026-01-22 18:21:30 -0300

    testando links

--- projetos/hub-evolutorio/funcoes_matematicas.js
+++ projetos/hub-evolutorio/funcoes_matematicas.js
@@ -1,8 +1,12 @@
-function calcularFuncao() {
+function calcularFuncaoDoPrimeiroGrau() {
+
+}
+
+function calcularFuncaoQuadratica() {
     const valorDeEntrada = Number(document.getElementById("valorDeEntrada").value);
     const coeficienteQuadratico = Number(document.getElementById("coeficienteQuadratico").value);
     const coeficienteLinear = Number(document.getElementById("coeficienteLinear").value);
     const termoIndependente = Number(document.getElementById("termoIndependente").value);
-    const resultadoDaFuncao = coeficienteQuadratico * (valorDeEntrada ** 2) + coeficienteLinear * valorDeEntrada + termoIndependente
-    document.getElementById("resultadoDaFuncao").textContent = resultadoDaFuncao
+    const resultadoDaFuncao = coeficienteQuadratico * (valorDeEntrada ** 2) + coeficienteLinear * valorDeEntrada + termoIndependente;
+    document.getElementById("resultadoDaFuncao").textContent = resultadoDaFuncao;
 }
