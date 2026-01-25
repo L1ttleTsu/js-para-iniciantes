2ce4434d552f01b2f5db37dc2adfb098713ba3b1
Bruno Gonçalves Santos <littletsu28@gmail.com>
2026-01-24 22:07:38 -0300

    Novos exercicios

---
+++ estudos/operador-ternario/script.js
@@ -0,0 +1,13 @@
+/*O operador ternário tem esse nome porque ele possui
+3 operandos/coisas acontecendo dentro dele*/
+idade = 15;
+var podeBeber = (idade >= 18) ? "Pode beber" : "Não pode beber";
+// condição ? true : false
+/* você passa uma condição o pede o valor boleano dela, depois indica o que ele faz
+se for verdadeiro e o que faz se for falso.*/
+var scroll = 1000; 
+scroll += 500;
+
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito = (possuiCarro && possuiCasa) ? "Credito aprovado" : "Credito negado"
